84d11456ff6d3db0c7019edeca428514b22c5080
OlgaKupar <Oljalya.topolja@gmail.com>
2025-06-10 14:52:41 +0200

    clean up

--- src/main/java/UserData.java
+++ src/main/java/UserData.java
@@ -2,5 +2,4 @@ public class UserData {
     // data structure, so variable data are exposed on purpose
     public String startingWebsite;
     public int maxCrawlingDepth;
-    //public String targetLanguage;
 }
--- src/main/java/UserQuery.java
+++ src/main/java/UserQuery.java
@@ -1,26 +1,20 @@
 public class UserQuery {
+
     final StartingWebsite startingWebsite = new StartingWebsite();
     final CrawlingDepth crawlingDepth = new CrawlingDepth();
-    //final TargetLanguage targetLanguage = new TargetLanguage();
     final UserData userData = new UserData();
 
-
     public UserData getUserData() {
-
         printWelcome();
 
         userData.startingWebsite = getStartingWebsiteFromUser();
         userData.maxCrawlingDepth = getCrawlingDepthFromUser();
-        //userData.targetLanguage = getTargetLanguageFromUser();
-
-        // summaryOfUserInput(); // todo: side-effect, delete later
 
         return userData;
     }
     public void printWelcome() {
         System.out.println("\nWelcome to WebCrawler\n");
-        System.out.println("Please enter a website to start and ");
-        System.out.println("the depth of websiteNode to crawl");
+        System.out.println("Please enter a website to start and the depth of websiteNode to crawl.");
     }
     public String getStartingWebsiteFromUser() {
         return startingWebsite.getStartingWebsiteFromUser();
@@ -30,15 +24,4 @@ public class UserQuery {
         return crawlingDepth.getCrawlingDepthFromUser();
     }
 
-/**
-    public String getTargetLanguageFromUser() {
-        return targetLanguage.getTargetLanguageFromUser();
-    }**/
-
-/**
-protected String summaryOfUserInput() {
-         return "\nStarting website: " + userData.startingWebsite +
-                ", crawling depth: " + userData.maxCrawlingDepth;
-    }
- **/
 }
