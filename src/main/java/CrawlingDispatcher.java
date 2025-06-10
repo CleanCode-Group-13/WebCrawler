6fc1a838f7424d141af953b372e3b085f9dc86ed
OlgaKupar <Oljalya.topolja@gmail.com>
2025-06-10 14:48:20 +0200

    refactored to enable concurrent crawling dependecies ommited; websites crawled in parallel; report is generated only when all threads have finished.

--- src/main/java/CrawlingDispatcher.java
+++ src/main/java/CrawlingDispatcher.java
@@ -1,65 +1,87 @@
-
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 
 public class CrawlingDispatcher {
-    UserData userData;
-    WebsiteNode rootNode;
-    HashSet<String> crawledUrls = new HashSet<>();
-    JsoupDocumentFetcher jsoupDocumentFetcher = new DocumentFetcher();
+    private UserData userData;
+    private WebsiteNode rootNode;
+    private Set<String> crawledUrls = Collections.synchronizedSet(new HashSet<>());
+    private JsoupDocumentFetcher jsoupDocumentFetcher = new DocumentFetcher();
+    private static final int THREAD_POOL_SIZE = 15; // works the best for my system
+    private ExecutorService executorService;
 
-    public CrawlingDispatcher(UserData userData) {
+    public CrawlingDispatcher(UserData userData, JsoupDocumentFetcher jsoupDocumentFetcher) {
         this.userData = userData;
+        this.executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+        this.jsoupDocumentFetcher = jsoupDocumentFetcher;
+    }
+
+    protected WebCrawler createWebCrawler(String url, JsoupDocumentFetcher fetcher) {
+        return new WebCrawler(url, fetcher);
     }
 
     public void crawlWeb() {
-        WebCrawler webCrawler = new WebCrawler(userData.startingWebsite, jsoupDocumentFetcher);
+        WebCrawler webCrawler = createWebCrawler(userData.startingWebsite, jsoupDocumentFetcher);
         Website website = webCrawler.getWebsiteHeadingsAndLinks();
         rootNode = new WebsiteNode();
         rootNode.setWebsite(website);
         crawledUrls.add(userData.startingWebsite);
 
         if (website != null) {
-            crawlRecursively(rootNode, 1);
+            CompletableFuture<Void> allCrawlTasks = crawlRecursivelyAndReturnFuture(rootNode, 1);
+            allCrawlTasks.join();
         }
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println("Crawling was interrupted..");
+        }
+        System.out.println("All links were successfully crawled. Report is being generated...");
     }
 
-    private void crawlRecursively(WebsiteNode websiteNode, int currentCrawlingDepth) {
-        if (currentCrawlingDepth > userData.maxCrawlingDepth) {
-            return;
+    private CompletableFuture<Void> crawlRecursivelyAndReturnFuture(WebsiteNode websiteNode, int currentCrawlingDepth) {
+        if (currentCrawlingDepth > userData.maxCrawlingDepth || websiteNode.getWebsite() == null) {
+            return CompletableFuture.completedFuture(null);
         }
-
         ArrayList<String> links = websiteNode.getWebsite().functionalLinks;
+        List<CompletableFuture<Void>> childCrawlFutures = new ArrayList<>();
 
-        List<CompletableFuture<WebsiteNode>> linksFutures = links.stream()
-                .filter(link -> !crawledUrls.contains(link))
-                .map(link -> {
-                    crawledUrls.add(link);
-                    return CompletableFuture.supplyAsync(() -> {
-                        WebCrawler webCrawler = new WebCrawler(link, jsoupDocumentFetcher);
-                        Website website = webCrawler.getWebsiteHeadingsAndLinks();
-                        WebsiteNode childNode = new WebsiteNode();
-                        childNode.setWebsite(website);
-                        return childNode;
-                    });
-                }).toList();
+        for (String link : links) {
+            if (!crawledUrls.contains(link)) {
+                crawledUrls.add(link);
 
-        CompletableFuture.allOf(linksFutures.toArray(new CompletableFuture[0])).join();
-
-        for (CompletableFuture<WebsiteNode> future : linksFutures) {
-            WebsiteNode childNode = future.join();
-            websiteNode.addChild(childNode);
-            crawlRecursively(childNode, currentCrawlingDepth + 1);
+                CompletableFuture<Void> linkCrawlFuture = CompletableFuture.supplyAsync(() -> {
+                    WebCrawler webCrawler = createWebCrawler(link, jsoupDocumentFetcher);
+                    Website website = webCrawler.getWebsiteHeadingsAndLinks();
+                    WebsiteNode childNode = new WebsiteNode();
+                    childNode.setWebsite(website);
+                    return childNode;
+                }, executorService).thenCompose(childNode -> {
+                    if (childNode.getWebsite() != null) {
+                        websiteNode.addChild(childNode);
+                        return crawlRecursivelyAndReturnFuture(childNode, currentCrawlingDepth + 1);
+                    } else {
+                        return CompletableFuture.completedFuture(null);
+                    }
+                });
+                childCrawlFutures.add(linkCrawlFuture);
+            }
         }
+        return CompletableFuture.allOf(childCrawlFutures.toArray(new CompletableFuture[0]));
     }
 
     public WebsiteNode getRootNode() {
         return rootNode;
     }
 }
-
-
 
 
